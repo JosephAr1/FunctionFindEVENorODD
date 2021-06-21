7634d80d7da0c9c4c1ce57589b2234baf6258fb1
JosephAr1 <jonielhet@yahoo.com>
2021-06-21 20:15:26 +1200

    Add project files.

---
+++ FunctionFindEVENorODD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FunctionFindEVENorODD
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int n; 
+
+            Console.WriteLine("Please enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            findoddoreven(n);
+
+
+        }
+        static void findoddoreven(int n)
+        {
+            if(n% 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
